c6b0efbfdd27504a522bfab1c04228cb054774a2
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-06 16:50:20 -0600

    ejercicios

---
+++ ejercicio1.php
@@ -0,0 +1,7 @@
+<?php 
+    echo "Hola Maria, aqui en PHP";
+
+    print_r("Maria Luisa ese trabajo del Martes es tuyo, Dios te lo dara, mantente firme");
+
+    print(" Otra prueba mas Maria Luisa, Paciencia...");
+?>

0d7d85372f38be0814d6f77d5e19b002f268f047
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-06 20:03:15 -0600

    Defining constants

---
+++ ejercicio7.php
@@ -0,0 +1,8 @@
+
+<?php
+
+    define("NAME", "Maria");  //here we can define a constant, NAME is the name of variable, and Maria is the value.
+
+    echo NAME; //Print the value that have the constant NAME. NAME never can be annother value because is a constant.
+
+?>

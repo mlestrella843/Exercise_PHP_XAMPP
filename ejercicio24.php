3460fc7b5a8002fb886bb041d218fda08d8b8808
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-08 12:27:09 -0600

    Inserting data into fotos table

--- ejercicio24.php
+++ ejercicio24.php
@@ -1,21 +1,24 @@
 <?php 
 
-
     $server="localhost"; //127.0.0.1
     $user="root";
     $password="";
 
     try{
 
-        $connection= new PDO("mysql:host=$server;dbname:album", $user, $password);
+        $connection= new PDO("mysql:host=$server;dbname=album", $user, $password);
         $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        echo "Connection established";
 
+        //Insert data into table fotos.
+        $sql="INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, 'playing with programming', 'foto.jpg');";
+
+        //Executes tha SQL instruction
+        $connection->exec($sql);
+
+        echo "Connection established";
 
     }catch(PDOException $error){
         echo "Connection wrong".$error;
-
     }
-
 
 ?>

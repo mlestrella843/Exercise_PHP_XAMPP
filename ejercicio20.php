f78d50bfde02538a9423f30f0d0f64b900f948d1
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-07 19:43:02 -0600

    php functions for arrays

---
+++ ejercicio20.php
@@ -0,0 +1,11 @@
+<?php 
+    
+     $frutas = array("Fresa", "Manzana", "Uvas");
+ 
+     //Funcion array_push , perteneciente a php
+     array_push($frutas, "Mandarina");
+
+     print_r($frutas);
+
+
+?>

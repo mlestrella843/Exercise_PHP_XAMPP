2a85ae48a79542f35823b93cc7b9f861a4fdcc5c
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-14 11:36:06 -0600

    adding checboxes inputs

--- receptionForm2.php
+++ receptionForm2.php
@@ -1,11 +1,27 @@
 <?php
-
     $txtName="";
     $rdgLenguage="";
+    $chkphp="";
+    $chkhtml="";
+    $chkcss="";
 
     if($_POST){
         $txtName=( isset( $_POST['txtName']))?$_POST['txtName']:"";
         $rdgLenguage=( isset( $_POST['language']))?$_POST['language']:"";
+
+        $chkphp=( isset( $_POST['chkphp'])=="yes")?"checked":"";
+        $chkhtml=( isset( $_POST['chkhtml'])=="yes")?"checked":"";
+        $chkcss=( isset( $_POST['chkcss'])=="yes")?"checked":"";
+
+        if($chkphp){
+            $var1 =  "I'm learning PHP now";
+        }
+        if($chkhtml){
+            $var2 =  "I'm learning HTML now";
+        }
+        if($chkcss){
+            $var3 =  "I'm learning CSS now";
+        }
     }
 ?>
 
@@ -23,6 +39,14 @@
     <br/>
     <strong>Your favorite language is: </strong>: <?php echo $rdgLenguage; ?>
     <br/>
+    <strong>And I'm learning those languages: </strong>:
+    <br/>
+    <?php echo $var1; ?>
+    <br/>
+    <?php echo $var2; ?>
+    <br/>
+    <?php echo $var3; ?>
+    <br/>
     <?php } ?>
 
     <form action="receptionForm2.php" method="post">
@@ -33,6 +57,11 @@
         <br/> php: <input type="radio" <?php echo ($rdgLenguage=="php")?"checked":"";?> name="language" value="php" id=""><br/>
         <br/> html: <input type="radio" <?php echo ($rdgLenguage=="html")?"checked":"";?> name="language" value="html" id=""><br/>
         <br/> css: <input type="radio" <?php echo ($rdgLenguage=="css")?"checked":"";?> name="language" value="css" id=""><br/>
+
+        You are learning those languages...<br/>
+        <br/> php: <input type="checkbox" <?php echo $chkphp;?> name="chkphp" value="yes" id=""><br/>
+        <br/> html: <input type="checkbox" <?php echo $chkhtml;?> name="chkhtml" value="yes" id=""><br/>
+        <br/> css: <input type="checkbox" <?php echo $chkcss;?> name="chkcss" value="yes" id=""><br/>
 
         <input type="submit" value="Send Information">
     </form> 

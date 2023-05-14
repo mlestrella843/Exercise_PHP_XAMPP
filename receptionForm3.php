907963be60932f103ec79f68ca8c313c95a2a71f
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-14 15:38:44 -0600

    Adding Select Option

--- receptionForm3.php
+++ receptionForm3.php
@@ -1,9 +1,12 @@
 <?php
     $txtName="";
     $rdgLenguage="";
+
     $chkphp="";
     $chkhtml="";
     $chkcss="";
+
+    $lsAnime="";
 
     if($_POST){
         $txtName=( isset( $_POST['txtName']))?$_POST['txtName']:"";
@@ -13,15 +16,9 @@
         $chkhtml=( isset( $_POST['chkhtml'])=="yes")?"checked":"";
         $chkcss=( isset( $_POST['chkcss'])=="yes")?"checked":"";
 
-        if($chkphp){
-            $var1 =  "I'm learning PHP now";
-        }
-        if($chkhtml){
-            $var2 =  "I'm learning HTML now";
-        }
-        if($chkcss){
-            $var3 =  "I'm learning CSS now";
-        }
+        $lsAnime=( isset( $_POST['lsAnime']) )?$_POST['lsAnime']:"";
+       // print_r($lsAnime)."<br/>";
+        
     }
 ?>
 
@@ -34,35 +31,55 @@
     <title>Exercises PHP</title>
 </head>
 <body>
+    <br/>
     <?php  if($_POST) { ?> 
     <strong>Hello</strong>: <?php echo $txtName; ?>
     <br/>
-    <strong>Your favorite language is: </strong>: <?php echo $rdgLenguage; ?>
+    <br/>
+    <strong>My favorite language is: </strong>: <?php echo $rdgLenguage; ?>
+    <br/>
     <br/>
     <strong>And I'm learning those languages: </strong>:
     <br/>
-    <?php echo $var1; ?>
+    <?php echo ($chkphp=="checked")?"PHP":"";  ?> 
+    <?php echo ($chkhtml=="checked")?"HTML":"";  ?>  
+    <?php echo ($chkcss=="checked")?"CSS":"";  ?>  
+    <br/> 
     <br/>
-    <?php echo $var2; ?>
-    <br/>
-    <?php echo $var3; ?>
+    <strong>My favorite Anime is: </strong>: <?php echo $lsAnime; ?>  
+    <br/> 
     <br/>
     <?php } ?>
-
-    <form action="receptionForm2.php" method="post">
+   
+    <form action="receptionForm3.php" method="post">
         Name: <br/>
         <input value="<?php echo $txtName; ?>" type="text" name="txtName" id="">
+        <br/>
         <br/>
         What do you prefer? <br/>
         <br/> php: <input type="radio" <?php echo ($rdgLenguage=="php")?"checked":"";?> name="language" value="php" id=""><br/>
         <br/> html: <input type="radio" <?php echo ($rdgLenguage=="html")?"checked":"";?> name="language" value="html" id=""><br/>
         <br/> css: <input type="radio" <?php echo ($rdgLenguage=="css")?"checked":"";?> name="language" value="css" id=""><br/>
+        <br/>
+        <br/>
 
         You are learning those languages...<br/>
         <br/> php: <input type="checkbox" <?php echo $chkphp;?> name="chkphp" value="yes" id=""><br/>
         <br/> html: <input type="checkbox" <?php echo $chkhtml;?> name="chkhtml" value="yes" id=""><br/>
         <br/> css: <input type="checkbox" <?php echo $chkcss;?> name="chkcss" value="yes" id=""><br/>
+        <br/>
+        <br/>
 
+        Which Anime do you like? .<br/>
+        <select name="lsAnime" id="">
+            <option value="">[Ninguna serie]</option>
+            <option value="naruto" <?php echo($lsAnime=="naruto")?"selected":""; ?> >Naruto</option>
+            <option value="bleach" <?php echo($lsAnime=="bleach")?"selected":""; ?> >Bleach</option>
+            <option value="dragon" <?php echo($lsAnime=="dragon")?"selected":""; ?> >Dragon Ball</option>
+        </select>
+        <br/>
+        <br/>
+        <br/>
         <input type="submit" value="Send Information">
     </form> 
 </body>
